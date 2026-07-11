23f3aaa37538a433b0d9e4f7c580fb33d3a64fab
elomordoxdxd55-wq <elomordoxdxd55@gmail.com>
2026-07-11 02:07:50 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import { defineConfig } from 'vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/NAZWA_REPO/",
+  base: "/staty/",
   plugins: [
     base44({
       legacySDKImports: process.env.BASE44_LEGACY_SDK_IMPORTS === 'true',
